3c5ab5b3eb8626f66ca1dc4026385092bab1dfeb
SONICBOOM2022 <118233824+SONICBOOM2022@users.noreply.github.com>
2022-12-08 06:49:42 +0800

    Updates

--- December/12022022/index.js
+++ December/12022022/index.js
@@ -1,38 +1,10 @@
-// Display Functions
-// alert("Gumising Ka Jam!");
-// console.log("Hello Console!");
-// Variation
-let isApproved = true;    // Boolean Literal
-var person = "John";     // string data type / String Literal
-console.log(person);
-
-
-let age = 12;       //  number data type / Number Literal
-age = 15;
-console.log(age);
-
-const birthplace = "Quezon City";
-console.log(birthplace);
-
-//  Data Types
-const firstName = "Jose";   // String Data type, String literal
-const lastName = 'Rizal';
-// Backtick
-const message = `Jose Rizal
-                                this is another line
-one more line`;  // String Data type, String literal
-
-console.log(message);
-
-
-
 let num1 = 1;
 let printValue;
 num1 = 21;
 
 if (num1 > 10 && num1 < 20) {
     printValue = "The value of num1 is greater than 10 and less than 20";
-}
+} 
 else if (num1 == 10 || num1 == 21) {
     printValue = "The value of num1 is equal to 10 or equal to 21";
 }
@@ -44,41 +16,37 @@ else {
 }
 console.log(printValue);
 
-
-
-//let printStatus;
-//const age = 16;
-
+// 
+let printStatus;
+const age = 19;
 // if(age >= 18) {
-//    printStatus = "Approved";
-//}
-//else {
-//    printStatus = "Disapproved";    
+//     printStatus = "Approve";
+// }
+// else {
+//     printStatus = "Disapprove";
 // }
 
-
-// Ternary Operator ? :
-// (condition) ? (value if true) : (value if false);
-printStatus = (age >= 18) ? "Approved" : "Disapproved";
+// Ternary operator ? :
+// (condition) ? {value if true} : {value if false};
+printStatus = (age >= 18) ? "Approve" : "Disapprove";
 console.log(printStatus);
 
 
-
-
-const hourOfDay = prompt("Endter hour of day");
+// const hourOfDay = prompt("Enter hour of day");
+const hourOfDay = new Date().getHours();
 let greeting;
 
 if (hourOfDay >= 4 && hourOfDay < 12) {
-    greeting = "Good Morning";
+    greeting = "Good Morning!";
 }
 else if (hourOfDay >= 12 && hourOfDay <= 18) {
-    greeting = "Good Afternoon";
+    greeting = "Good Afternoon!";
 }
 else {
-    greeting = "Good Eveming";
+    greeting = "Good Evening!";
 }
 
-
+console.log(hourOfDay);
 console.log(greeting);
 
 
@@ -96,4 +64,30 @@ switch(day) {
 
     default:
         console.log("Today is unknown");
+}
+
+
+// Loops
+
+let num = 0;
+
+// while (num < 10) { // condition
+//     console.log(num);
+//     num++; // increment, num = num + 1
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// } 
+// while (num < 10);
+
+
+// (initialization; condition; post-loop operation)
+for (let i = 0; i < 10; i++) {
+    
+    // check if number is even
+    if(i % 2 == 0) {
+        console.log(i);
+    }
 }
